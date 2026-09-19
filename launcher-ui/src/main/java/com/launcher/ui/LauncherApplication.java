4e2a0f267e790162bb44a1534debaf2ca9771080
Dmitro Vasyliev <dmitriyvas21@gmail.com>
2026-09-19 13:12:33 +0300

    refactor(ui): simplify reserved UI skeleton

--- launcher-ui/src/main/java/com/launcher/ui/LauncherApplication.java
+++ launcher-ui/src/main/java/com/launcher/ui/LauncherApplication.java
@@ -1,20 +1,25 @@
 package com.launcher.ui;
 
-import com.launcher.ui.config.DefaultConfigValues;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class LauncherApplication extends Application {
-    private static final String APPLICATION_LABEL_NAME = "My little launcher";
+    private static final String APPLICATION_NAME = "My little launcher";
+    private static final int DEFAULT_WINDOW_WIDTH = 900;
+    private static final int DEFAULT_WINDOW_HEIGHT = 550;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        final Label label = new Label(APPLICATION_LABEL_NAME);
-        final Scene scene = new Scene(label, DefaultConfigValues.width, DefaultConfigValues.height);
+    public void start(Stage primaryStage) {
+        final Label label = new Label(APPLICATION_NAME);
+        final Scene scene = new Scene(
+                label,
+                DEFAULT_WINDOW_WIDTH,
+                DEFAULT_WINDOW_HEIGHT
+        );
 
-        primaryStage.setTitle(APPLICATION_LABEL_NAME);
+        primaryStage.setTitle(APPLICATION_NAME);
         primaryStage.setScene(scene);
 
         primaryStage.show();
--- launcher-ui/src/main/java/com/launcher/ui/config/DefaultConfigValues.java
+++
@@ -1,7 +0,0 @@
-package com.launcher.ui.config;
-
-public class DefaultConfigValues {
-    public static final int width = 900;
-    public static final int height = 550;
-    private DefaultConfigValues () {}
-}
